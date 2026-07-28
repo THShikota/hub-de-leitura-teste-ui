f7a28d690267a68cadab6108a59bdc5920a5e0d0
Tiago Shikota <th.shikota@gmail.com>
2026-07-28 09:57:45 -0300

    Criação dos cenários negativos para cada campo obrigatório do formulário.

--- cypress/e2e/contato.cy.js
+++ cypress/e2e/contato.cy.js
@@ -1,12 +1,48 @@
 describe('Funcionalidade: Contato', () => {
+
+  beforeEach(() => {
+    cy.visit('index.html');
+  });
+
   it('Deve preencher o formulário de contato com sucesso', () => {
-    //Aqui vai ser implementado o primeiro teste
-    cy.visit('http://localhost:3000/index.html');
     cy.get('[name="name"]').type('Tiago Shikota');
     cy.get('[name="email"]').type('tiago@teste.com');
     cy.get('[name="subject"]').select('Sugestões');
     cy.get('[name="message"]').type('Mensagem de teste');
     cy.get('#btn-submit').click();
+    // Resultado esperado: exibir a mensagem de sucesso "Contato enviado com sucesso!"
     cy.contains('Contato enviado com sucesso!').should('exist');
-  })
+  });
+
+  it('Deve exibir mensagem de erro ao enviar o formulário sem preencher o campo obrigatório nome', () => {
+    cy.get('[name="email"]').type('tiago@teste.com');
+    cy.get('[name="subject"]').select('Sugestões');
+    cy.get('[name="message"]').type('Mensagem de teste');
+    cy.get('#btn-submit').click();
+    cy.get('#alert-container').should('contain', 'Por favor, preencha o campo Nome.');
+  });
+
+  it('Deve exibir mensagem de erro ao enviar o formulário sem preencher o campo obrigatório email', () => {
+    cy.get('[name="name"]').type('Tiago Shikota');
+    cy.get('[name="subject"]').select('Sugestões');
+    cy.get('[name="message"]').type('Mensagem de teste');
+    cy.get('#btn-submit').click();
+    cy.get('#alert-container').should('contain', 'Por favor, preencha o campo E-mail.');
+  });
+
+  it('Deve exibir mensagem de erro ao enviar o formulário sem preencher o campo obrigatório assunto', () => {
+    cy.get('[name="name"]').type('Tiago Shikota');
+    cy.get('[name="email"]').type('tiago@teste.com');
+    cy.get('[name="message"]').type('Mensagem de teste');
+    cy.get('#btn-submit').click();
+    cy.get('#alert-container').should('contain', 'Por favor, selecione o Assunto.');
+  });
+
+  it('Deve exibir mensagem de erro ao enviar o formulário sem preencher o campo obrigatório mensagem', () => {
+    cy.get('[name="name"]').type('Tiago Shikota');
+    cy.get('[name="email"]').type('tiago@teste.com');
+    cy.get('[name="subject"]').select('Sugestões');
+    cy.get('#btn-submit').click();
+    cy.get('#alert-container').should('contain', 'Por favor, escreva sua Mensagem.');
+  });
 });
